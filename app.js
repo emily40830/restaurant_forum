d2fde4e98d7c12230a566b477d3f7081fa329292
emily40830 <emily40830@gmail.com>
2020-09-08 00:15:38 +0800

    feat:add signin signup routes to userController

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const exhbs = require('express-handlebars');
 const db = require('./models'); //引入資料庫
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 const routes = require('./routes');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js');
 const adminController = require('../controllers/adminController.js');
+const userController = require('../controllers/userController');
 
 const main = (app) => {
   app.get('/', (req, res) => res.redirect('/restaurants'));
@@ -10,6 +11,10 @@ const main = (app) => {
 
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
   app.get('/admin/restaurants', adminController.getRestaurants);
+
+  // 登入/註冊
+  app.get('/signup', userController.signUpPage);
+  app.post('/signup', userController.signUp);
 };
 
 module.exports = main;
