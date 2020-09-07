536b239edba75f954fb5a3c27f72429f5dfc114d
emily40830 <emily40830@gmail.com>
2020-09-07 21:31:23 +0800

    Add admin restaurant index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
-const restController = require('../controllers/restController');
+const restController = require('../controllers/restController.js');
+const adminController = require('../controllers/adminController.js');
 
 const main = (app) => {
   app.get('/', (req, res) => res.redirect('/restaurants'));
   app.get('/restaurants', restController.getRestaurants);
+
+  // 連到admin頁面就轉到 /admin/restaurants
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'));
+
+  // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
+  app.get('/admin/restaurants', adminController.getRestaurants);
 };
 
 module.exports = main;
