66ac56d6aa6b6d13c103398f97fdac06c69eb099
emily40830 <emily40830@gmail.com>
2020-09-09 22:37:35 +0800

    feat:Install express-session connect-flash and add messages when signup

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 const exhbs = require('express-handlebars');
 const db = require('./models'); //引入資料庫
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 const app = express();
 const port = 3000;
 
 const routes = require('./routes');
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages');
+  res.locals.error_messages = req.flash('error_messages');
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Running on localhost:${port}`);
