cfe8a5350315206124b68edc47c4a3e9595c8a52
emily40830 <emily40830@gmail.com>
2020-09-12 14:55:38 +0800

    Add user seed data & Install faker and add restaurant seed data

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -11,8 +11,8 @@ const adminController = {
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, { raw: true }).then(
       (restaurant) => {
-        return res.render('admin/restaurant', () => {
-          restaurant: restaurant;
+        return res.render('admin/restaurant', {
+          restaurant: restaurant,
         });
       },
     );
