17e208d140dba34a8f91a59400f866b5f3b17541
emily40830 <emily40830@gmail.com>
2020-09-14 22:22:36 +0800

    feat:Add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,18 @@
+const db = require('../models');
+const Category = db.Category;
+const Restaurant = db.Restaurant;
+
 const restController = {
   // 負責瀏覽餐廳頁面的渲染
   getRestaurants: (req, res) => {
+    Restaurant.findAll({ include: Category }).then((restaurants) => {
+      const data = restaurants.map((r) => ({
+        ...r.dataValues, //展開物件資料
+        description: r.dataValues.description.substring(0, 50), //覆寫description
+        categoryName: r.Category.name,
+      }));
+      return res.render('restaurants', { restaurants: data });
+    });
     return res.render('restaurants');
   },
 };
