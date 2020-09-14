cfd0cb809b22ccae5c1575c57399266185efd585
emily40830 <emily40830@gmail.com>
2020-09-15 00:07:37 +0800

    feat:Add categories navbar on restaurants index page & add filter on category navbar

--- controllers/restController.js
+++ controllers/restController.js
@@ -5,14 +5,30 @@ const Restaurant = db.Restaurant;
 const restController = {
   // 負責瀏覽餐廳頁面的渲染
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then((restaurants) => {
-      const data = restaurants.map((r) => ({
-        ...r.dataValues, //展開物件資料
-        description: r.dataValues.description.substring(0, 50), //覆寫description
-        categoryName: r.Category.name,
-      }));
-      return res.render('restaurants', { restaurants: data });
-    });
+    let whereQuery = {};
+    let categoryId = '';
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.id);
+      whereQuery.CategoryId = categoryId;
+    }
+    Restaurant.findAll({ include: Category, where: whereQuery }).then(
+      (restaurants) => {
+        const data = restaurants.map((r) => ({
+          ...r.dataValues, //展開物件資料
+          description: r.dataValues.description.substring(0, 50), //覆寫description
+          categoryName: r.Category.name,
+        }));
+        return Category.findAll({ raw: true, nest: true }).then(
+          (categories) => {
+            return res.render('restaurants', {
+              restaurants: data,
+              categories,
+              categoryId,
+            });
+          },
+        );
+      },
+    );
   },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
@@ -20,7 +36,7 @@ const restController = {
       nest: true,
       include: Category,
     }).then((restaurant) => {
-      console.log(restaurant);
+      //console.log(restaurant);
       return res.render('restaurant', {
         restaurant,
       });
