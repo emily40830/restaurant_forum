f1a4a7fd4bcf8d7c2c31fc3188f4daa096d48d04
emily40830 <emily40830@gmail.com>
2020-09-20 17:40:30 +0800

    refactor adminController.deleteRestaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -142,11 +142,16 @@ const adminController = {
     }
   },
   deleteRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id).then((restaurant) => {
-      restaurant.destroy().then(() => {
+    adminService.deleteRestaurant(req, res, (data) => {
+      if (data['status'] === 'success') {
         res.redirect('/admin/restaurants');
-      });
+      }
     });
+    // return Restaurant.findByPk(req.params.id).then((restaurant) => {
+    //   restaurant.destroy().then(() => {
+    //     res.redirect('/admin/restaurants');
+    //   });
+    // });
   },
   getUsers: (req, res) => {
     return User.findAll({ raw: true }).then((users) => {
